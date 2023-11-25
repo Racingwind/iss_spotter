01205bb254f1bc7ad546475257fb4670fbfee2da
Racingwind <louis.k.hu@gmail.com>
2023-11-25 00:21:19 -0800

    able to fetch IP address data in json format

---
+++ iss.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) { 
+
+}
+
+const url = "https://api.ipify.org?format=json";
+request(url, (error, response, body) => {
+  console.log(error);
+  console.log(body);
+  // callback(error, desc);
+
+});
+
+
+module.exports = { fetchMyIP };
+
+
+// {"ip":"69.172.160.70"}
